ab1df84c6ba309ea27870e5384a11f99fd1c6616
Adyoshkin Vasiliy <adeshkin.vi@phystech.edu>
2021-04-27 19:44:00 +0300

    add dataset

--- demo/train.py
+++ demo/train.py
@@ -1,19 +1,13 @@
-import sys
-sys.path.append("../mcts_dl")
-
 import torch
 import torch.optim as optim
 from collections import defaultdict
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
 import copy
 import time
 import numpy as np
-import os
 
 from .model import UNet
-from utils.utils import ReadMapFromMovingAIFile, Map
 
 
 def dice_loss(pred, target, smooth=1.):
@@ -124,36 +118,7 @@ def train_model(model, optimizer, dataloaders, input_data, num_epochs, offset, d
     return model
 
 
-def get_image_map(gridMap: Map):
-    hIm = gridMap.height
-    wIm = gridMap.width
-    im = np.zeros((hIm, wIm), dtype=np.uint8)
-    for i in range(gridMap.height):
-        for j in range(gridMap.width):
-            if (gridMap.cells[i][j] == 1):
-                im[i][j] = 1
-    return np.asarray(im)
 
-
-class City(Dataset):
-    def __init__(self, map_root):
-        map_paths = [os.path.join(map_root, path)
-                     for path in os.listdir(map_root)
-                     if ".map" in path]
-
-        task_maps = [ReadMapFromMovingAIFile(map_path)
-                     for map_path in map_paths]
-
-        self.image_maps = [get_image_map(task_map)
-                           for task_map in task_maps]
-
-    def __len__(self):
-        return len(self.image_maps)
-
-    def __getitem__(self, idx):
-        image_map = self.image_maps[idx]
-
-        return ToTensor()(image_map)
 
 
 def main():
