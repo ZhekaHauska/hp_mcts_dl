8bb00bdd0b3c35c03f45d13c0920a98a370dbb6e
adeshkin <adeshkin.vi@phystech.edu>
2021-05-05 16:11:44 +0300

    add log image

--- mcts_dl/algorithms/model_network.py
+++ mcts_dl/algorithms/model_network.py
@@ -22,38 +22,57 @@ def calc_iou(outputs, targets):
 
 
 class ModelNetwork(nn.Module):
-    def __init__(self, ):
+    def __init__(self, window_size):
         super(ModelNetwork, self).__init__()
         self.input = nn.Sequential(
             nn.Conv2d(1, 4, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(4, 8, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+
+        self.action = nn.Sequential(
+            nn.Linear(2, 128),
             nn.ReLU(),
-            nn.Conv2d(8, 4, kernel_size=3, padding=1),
+            nn.Linear(128, window_size*window_size),
+            nn.ReLU()
+        )
+
+        self.output = nn.Sequential(
+            nn.Conv2d(8+1, 4, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(4, 1, kernel_size=3, padding=1),
             nn.Sigmoid()
         )
-        self.action = nn.Sequential(
-            nn.Linear(2, 100),
-        )
 
     def forward(self, inputs, actions):
-        return self.model(x)
+        inputs_ = self.input(inputs)
+        actions_ = self.action(actions)
+        actions_ = actions_.view(inputs.shape)
+
+        outputs = torch.cat((inputs_, actions_), dim=1)
+        outputs = self.output(outputs)
+
+        return outputs
 
 
 class Runner:
     def __init__(self, data_set):
-        self.model = ModelNetwork()
+        self.offset = 10
+
+        window_size = 2 * self.offset + 1
+        self.model = ModelNetwork(window_size)
+
+        self.loss_func = nn.BCELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)
+
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.batch_size = 4
+        self.batch_size = 10
         self.num_epochs = 100
         self.data_loaders = {'train': DataLoader(data_set['train'], batch_size=self.batch_size, shuffle=True),
                              'val': DataLoader(data_set['val'], batch_size=self.batch_size, shuffle=False)}
-        self.offset = 21
-        self.num_steps = 1000
-        self.loss_func = nn.BCELoss()
+
+        self.num_steps = 100
 
     # check random seed
     def sample(self, image_map):
@@ -61,13 +80,15 @@ class Runner:
         inputs = image_map[:, :, (y0 - self.offset):(y0 + self.offset + 1), (x0 - self.offset):(x0 + self.offset + 1)]
 
         targets = torch.zeros_like(inputs)
-        actions = torch.zeros(inputs.shape[0])
+        actions = torch.zeros((inputs.shape[0], 2))
+
         for i in range(inputs.shape[0]):
             dy, dx = np.random.randint(-1, 2, size=(1, 2))[0]
             y = y0 + dy
             x = x0 + dx
             targets[i] = image_map[i, :, (y - self.offset):(y + self.offset + 1), (x - self.offset):(x + self.offset + 1)]
-            actions[i] = torch.tensor([dy, dx], dtype=torch.int8)
+            actions[i][0] = dy
+            actions[i][1] = dx
 
         return inputs, targets, actions
 
@@ -127,7 +148,7 @@ class Runner:
         epoch_loss = epoch_loss / (len(self.data_loaders['val']) * self.num_steps * self.batch_size)
         epoch_iou = epoch_iou / (len(self.data_loaders['val']) * self.num_steps * self.batch_size)
 
-        return epoch_loss, epoch_iou
+        return epoch_loss, epoch_iou, outputs, targets
 
     def run(self):
         best_model_wts = copy.deepcopy(self.model.state_dict())
@@ -137,7 +158,7 @@ class Runner:
 
         for epoch in range(self.num_epochs):
             train_loss, train_iou = self.train()
-            val_loss, val_iou = self.eval()
+            val_loss, val_iou, outputs, targets = self.eval()
 
             logs = {'train_loss': train_loss,
                     'train_iou': train_iou,
@@ -148,6 +169,8 @@ class Runner:
             if val_loss < best_loss:
                 best_loss = val_loss
                 best_model_wts = copy.deepcopy(self.model.state_dict())
+                wandb.log({f"epoch = {epoch}": [wandb.Image(im, caption=c) for im, c in zip([outputs, targets],
+                                                                                            ['pred', 'true'])]})
 
         self.model.load_state_dict(best_model_wts)
         return self.model
@@ -155,6 +178,7 @@ class Runner:
 
 if __name__ == '__main__':
     wandb.init(project="mcts-dl")
+
     city_ds = City(map_root="../../data/street-map")
     data_set = {'train': city_ds,
                 'val': city_ds}
