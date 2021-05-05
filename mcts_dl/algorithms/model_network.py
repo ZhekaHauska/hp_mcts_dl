53fdad9a43e4a83c236b04658424f5cf557d643b
Adyoshkin Vasiliy <adeshkin.vi@phystech.edu>
2021-05-05 14:24:11 +0300

    add model network pipeline

---
+++ mcts_dl/algorithms/model_network.py
@@ -0,0 +1,158 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import copy
+import numpy as np
+import wandb
+
+from mcts_dl.utils.dataset import City
+
+
+def calc_iou(outputs, targets):
+    SMOOTH = 1e-6
+    outputs = outputs.squeeze(1) > 0.5
+    targets = targets.squeeze(1) > 0.5
+
+    intersection = (outputs & targets).float().sum((1, 2))
+    union = (outputs | targets).float().sum((1, 2))
+
+    iou = intersection / (union + SMOOTH)
+
+    return iou.sum()
+
+
+class ModelNetwork(nn.Module):
+    def __init__(self, ):
+        super(ModelNetwork, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 4, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(4, 8, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(8, 4, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(4, 1, kernel_size=3, padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Runner:
+    def __init__(self, data_set):
+        self.model = ModelNetwork()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.batch_size = 4
+        self.num_epochs = 100
+        self.data_loaders = {'train': DataLoader(data_set['train'], batch_size=self.batch_size, shuffle=True),
+                             'val': DataLoader(data_set['val'], batch_size=self.batch_size, shuffle=False)}
+        self.offset = 21
+        self.num_steps = 100
+        self.loss_func = nn.BCELoss()
+
+    # check random seed
+    def sample(self, image_map):
+        y0, x0 = np.random.randint(self.offset + 1, 256 - self.offset - 1, size=(1, 2))[0]
+        inputs = image_map[:, :, (y0 - self.offset):(y0 + self.offset + 1), (x0 - self.offset):(x0 + self.offset + 1)]
+
+        dy, dx = np.random.randint(-1, 2, size=(1, 2))[0]
+        y = y0 + dy
+        x = x0 + dx
+        targets = image_map[:, :, (y - self.offset):(y + self.offset + 1), (x - self.offset):(x + self.offset + 1)]
+
+        return inputs, targets
+
+    def train(self):
+        self.model.train()
+
+        epoch_loss = 0.0
+        epoch_iou = 0.0
+        for batch in self.data_loaders['train']:
+            image_map = batch['image']
+            for step in range(self.num_steps):
+                inputs, targets = self.sample(image_map)
+
+                inputs = inputs.to(self.device)
+                targets = targets.to(self.device)
+
+                outputs = self.model(inputs)
+
+                loss = self.loss_func(outputs, targets)
+                epoch_loss += loss.cpu().detach()
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                iou = calc_iou(outputs.cpu().detach(), targets.cpu().detach())
+                epoch_iou += iou
+
+        epoch_loss = epoch_loss / (len(self.data_loaders['train']) * self.num_steps * self.batch_size)
+        epoch_iou = epoch_iou / (len(self.data_loaders['train']) * self.num_steps * self.batch_size)
+
+        return epoch_loss, epoch_iou
+
+    def eval(self):
+        self.model.eval()
+
+        epoch_loss = 0.0
+        epoch_iou = 0.0
+        for batch in self.data_loaders['val']:
+            image_map = batch['image']
+            for step in range(self.num_steps):
+                inputs, targets = self.sample(image_map)
+
+                inputs = inputs.to(self.device)
+                targets = targets.to(self.device)
+
+                outputs = self.model(inputs)
+
+                loss = self.loss_func(outputs, targets)
+                epoch_loss += loss.cpu().detach()
+
+                iou = calc_iou(targets.cpu().detach(), targets.cpu().detach())
+                epoch_iou += iou
+
+        epoch_loss = epoch_loss / (len(self.data_loaders['val']) * self.num_steps * self.batch_size)
+        epoch_iou = epoch_iou / (len(self.data_loaders['val']) * self.num_steps * self.batch_size)
+
+        return epoch_loss, epoch_iou
+
+    def run(self):
+        best_model_wts = copy.deepcopy(self.model.state_dict())
+        best_loss = 1e10
+
+        self.model = self.model.to(self.device)
+
+        for epoch in range(self.num_epochs):
+            train_loss, train_iou = self.train()
+            val_loss, val_iou = self.eval()
+
+            logs = {'train_loss': train_loss,
+                    'train_iou': train_iou,
+                    'val_loss': val_loss,
+                    'val_iou': val_iou}
+            wandb.log(logs)
+
+            if val_loss < best_loss:
+                best_loss = val_loss
+                best_model_wts = copy.deepcopy(self.model.state_dict())
+
+        self.model.load_state_dict(best_model_wts)
+        return self.model
+
+
+if __name__ == '__main__':
+    wandb.init(project="mcts-dl")
+    city_ds = City(map_root="../../data/street-map")
+    data_set = {'train': city_ds,
+                'val': city_ds}
+
+    runner = Runner(data_set)
+    best_model = runner.run()
+
+    torch.save(best_model.state_dict(), "models/best_model.pth")
+
--- mcts_dl/utils/dataset.py
+++ mcts_dl/utils/dataset.py
@@ -1,20 +1,17 @@
-import sys
-sys.path.append("../mcts_dl")
-
 import os
 import numpy as np
 from torchvision.transforms import ToTensor
 from torch.utils.data import Dataset
-from utils.utils import ReadMapFromMovingAIFile, Map
+from .utils import ReadMapFromMovingAIFile, Map
 
 
-def get_image_map(gridMap: Map):
+def grid_map2image(gridMap: Map):
     hIm = gridMap.height
     wIm = gridMap.width
     im = np.zeros((hIm, wIm), dtype=np.uint8)
-    for i in range(gridMap.height):
-        for j in range(gridMap.width):
-            if (gridMap.cells[i][j] == 1):
+    for i in range(hIm):
+        for j in range(wIm):
+            if gridMap.cells[i][j] == 1:
                 im[i][j] = 1
     return np.asarray(im)
 
@@ -26,8 +23,7 @@ class City(Dataset):
         task_maps = [ReadMapFromMovingAIFile(os.path.join(map_root, map_path))
                      for map_path in self.map_paths]
 
-        self.image_maps = [get_image_map(task_map)
-                           for task_map in task_maps]
+        self.image_maps = [grid_map2image(task_map) for task_map in task_maps]
 
     def __len__(self):
         return len(self.image_maps)
