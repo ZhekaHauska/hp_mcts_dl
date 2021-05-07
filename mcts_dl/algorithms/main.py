5a71f23bfa3fb752f0979039c963a89eb82f462d
adeshkin <adeshkin.vi@phystech.edu>
2021-05-07 13:09:48 +0300

    add model border

--- mcts_dl/algorithms/main.py
+++ mcts_dl/algorithms/main.py
@@ -6,7 +6,7 @@ def predict(inputs, actions):
     window_size = 21
     threshold = 0.5
     model_path = './checkpoints/best_model.pth'
-    model = ModelNetwork(window_size)
+    model = ModelNetworkWindow(window_size)
     model.load_state_dict(torch.load(model_path))
     model.eval()
     # inputs : [batch_size, 1, h, w], where 0-free, 1-obstacle
